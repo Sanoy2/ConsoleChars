b951031b5e39ce51d9e88d8d5defc0dafcd67481
Krzysztof_Tomkow <218514@student.pwr.edu.pl>
2019-12-22 20:47:46 +0100

    Added all words that are needed to write 'DUPA'

---
+++ ConsoleChars/Implementation/Characters/Letters/Character_50.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleChars.Implementation.Characters.Letters
+{
+    public class Character_50 : Character
+    {
+        // P
+        protected override IList<string> ToMediumString()
+        {
+            List<string> lines = new List<string>();
+
+            lines.Add("#### ");
+            lines.Add("#   #");
+            lines.Add("#### ");
+            lines.Add("#    ");
+            lines.Add("#    ");
+            lines.Add("     ");
+
+            return lines;
+        }
+    }
+}
