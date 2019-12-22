d7610001a5ef49a49cab88fb9788360f46b92c6c
Krzysztof_Tomkow <218514@student.pwr.edu.pl>
2019-12-22 14:06:38 +0100

    Printing word works, but it needs space between characters

---
+++ ConsoleCharsCheck/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ConsoleChars;
+using ConsoleChars.Implementation;
+using ConsoleChars.Interfaces;
+
+namespace ConsoleCharsCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var checker = new SupportedCharactersChecker();
+            var factory = new CharacterFactory(checker);
+
+            var C = factory.Create('C');
+            var H = factory.Create('H');
+
+            ISentence text = new Sentence("CH");
+
+            Console.WriteLine(text.MediumString);
+
+            //Console.WriteLine(C.ToString());
+            //Console.WriteLine(H.ToString());
+        }
+    }
+}
